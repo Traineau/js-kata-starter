7544f6800168ee87a095dd4d7a7c30646ed7e712
Raineau Thomas <thomasraineau3@gmail.com>
2021-07-13 14:05:05 +0200

    FizzBuzz func with TU

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,36 @@
-import { systemUnderTest } from ".";
+import { fizzBuzz } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of fizzBuzz", () => {
+  it("Should return 'n' for n=4", () => {
+    const n = 4;
+    expect(fizzBuzz(n)).toEqual('4');
   });
+  it("Should return n for n=11", () => {
+    const n = 11;
+    expect(fizzBuzz(n)).toEqual('11');
+  })
+  it("Should return Fizz for n=3", () => {
+    const n = 3;
+    expect(fizzBuzz(n)).toEqual('Fizz');
+  })
+  it("Should return Fizz for n=6", () => {
+    const n = 6;
+    expect(fizzBuzz(n)).toEqual('Fizz');
+  })
+  it("Should return Buzz for n=5", () => {
+    const n = 5;
+    expect(fizzBuzz(n)).toEqual('Buzz');
+  })
+  it("Should return Buzz for n=10", () => {
+    const n = 10;
+    expect(fizzBuzz(n)).toEqual('Buzz');
+  })
+  it("Should return FizzBuzz for n=15", () => {
+    const n = 15;
+    expect(fizzBuzz(n)).toEqual('FizzBuzz');
+  })
+  it("Should return FizzBuzz for n=30", () => {
+    const n = 30;
+    expect(fizzBuzz(n)).toEqual('FizzBuzz');
+  })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function systemUnderTest(): boolean {
-  return true;
+export const fizzBuzz = (n: number) : string => {
+  let result = ''
+
+  if (isMultiple(3, n)) result += 'Fizz';
+  if (isMultiple(5, n)) result += 'Buzz'
+
+  if (result) {
+    return result;
+  }
+
+  return n.toString();
+}
+
+const isMultiple = (divider: number, n: number) : boolean => {
+  return n % divider == 0
 }
